da2b513ab0ad66ae1f7d62b5db09c6b8ef3158c4
valen <valen@DESKTOP-057E61O.lan>
2023-04-09 17:45:47 -0400

    new try of committed

---
+++ src/demo/Test.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Test {
+	
+	public static void main (String arg []) {
+		
+		System.out.println("Test ");
+	}
+	
+public void newTOpic(String test) {
+		
+		System.out.println("Second test ...");
+	}
+
+public void add(int a, int b) {
+	
+	System.out.println(a+b);
+}
+
+}
